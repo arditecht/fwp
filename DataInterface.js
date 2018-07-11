bc5f8b92da2df4b9770a34d1e3099c61e7a99350
coinerditecht <somjeet@coinconnect.in>
2018-07-11 12:58:10 +0530

    minor change

--- DataInterface.js
+++ DataInterface.js
@@ -3,6 +3,7 @@
 function DataInterface(){
 	/*
 	 * intercept all server calls, do manipulations, check connection, give offline readability
+	 * if not for the entire lifetime of the client usage, then for the same render cycle
 	 */
 	
 }
