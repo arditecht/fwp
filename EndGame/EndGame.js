3f94be9fc905bba3298c004bdbfc5d934e74cd4d
coinerditecht <somjeet@coinconnect.in>
2018-07-19 18:10:37 +0530

    something done

--- EndGame/EndGame.js
+++ EndGame/EndGame.js
@@ -1,12 +1,11 @@
-setTimeout(function(){
-    var editor = ace.edit("editor");
-    editor.setOptions({
-        fontSize: "12pt",
-        highlightGutterLine: true,
-        showFoldWidgets: true,
-        highlightActiveLine: true
-    });
-    editor.setTheme("ace/theme/ambiance");
-    editor.getSession().setMode("ace/mode/javascript");
-    editor.focus();
-}, 100);
+var editor;
+editor = ace.edit("editor");
+editor.setOptions({
+    fontSize: "12pt",
+    highlightGutterLine: true,
+    showFoldWidgets: true,
+    highlightActiveLine: true
+});
+editor.setTheme("ace/theme/ambiance");
+editor.getSession().setMode("ace/mode/javascript");
+editor.focus();
